d51117007189a671e10d51ac2aeb1f0dcf2d048d
marzelin <marzelin@gmail.com>
2017-02-14 21:19:38 +0100

    implement outline for main

---
+++ doing/break-the-pieces/main.ts
@@ -0,0 +1,12 @@
+// import { pipe } from 'ramda'
+
+// const breakPieces = 
+//   pipe(
+//     grid,
+//     (grid) => pipe(
+//       shapeCorners,
+//       origins(grid),
+//       piecesCorners(grid),
+//       pieces
+//     )(grid)
+//   )
